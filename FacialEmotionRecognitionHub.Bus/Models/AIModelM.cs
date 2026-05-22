02247cc3fa73ede397650b2590cd40fb904ade84
XINICOM <XINICOM@outlook.com>
2026-05-23 01:06:55 +0800

    DONE: 可变参数的函数框架

--- FacialEmotionRecognitionHub.Bus/Models/AIModelM.cs
+++ FacialEmotionRecognitionHub.Bus/Models/AIModelM.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FacialEmotionRecognitionHub.Bus.Services;
+using System.Dynamic;
 
 namespace FacialEmotionRecognitionHub.Bus.Models
 {
     public class AIModelM
     {
         public string Name;
+        private AIModelConnectionService _aIModelConnectionService;
+        private dynamic _instructionSet = new ExpandoObject();
 
-        public AIModelM(string name)
+        public AIModelM(AIModelConnectionService aIModelConnectionService, string instructionSet)
         {
-            Name = name;
+            //Name = name;
+            _aIModelConnectionService = aIModelConnectionService;
         }
     }
 }
---
+++ FacialEmotionRecognitionHub.Bus/Models/ModifiableInstruction.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+namespace FacialEmotionRecognitionHub.Bus.Models
+{
+    public delegate Task Command(Dictionary<string, object>? parameters);
+
+    public class ModifiableInstruction(string instructionName, Command command)
+    {
+        private string _instructionName = instructionName;
+        public string InstructionName
+        {
+            get { return _instructionName; }
+        }
+        public ModifiableInstruction? InstructionBefore;
+        private Dictionary<string, object>? _templateParameters;
+        public Dictionary<string, object>? TemplateParameters
+        {
+            get
+            {
+                return _templateParameters;
+            }
+        }
+        private bool _executable = false;
+        private Command command = command;
+        public async void Execute(Dictionary<string, object>? parameters = null)
+        {
+            if (!_executable || command is null)
+                return;
+            if(parameters is null)
+            {
+                if(TemplateParameters is null)
+                {
+                    await command(null);
+                    return;
+                }
+                else
+                {
+                    await command(TemplateParameters);
+                    return;
+                }
+            }
+            else
+            {
+                if(TemplateParameters is null)
+                {
+                    return;
+                }
+                else
+                {
+                    if (parameters.Keys.ToHashSet().SetEquals(TemplateParameters.Keys))
+                    {
+                        await command(parameters);
+                        return;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void AddParameter(string parameterName, object value)
+        {
+            if (_templateParameters is null)
+                _templateParameters = new();
+            _templateParameters.Add(parameterName, value);
+        }
+    }
+}
